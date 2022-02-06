eb8ddb30fbe98ec073d0f1db83a69b5bdcb6630e
mrSmithG <gbengamosun@gmail.com>
2022-02-06 18:31:27 +0100

    edited credentials.py file

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "here goes your acces token from BotFather"
 bot_user_name = "the username you entered"
-URL = "https://my-tele-bot-app.herokuapp.com/setwebhook"
+URL = "https://my-tele-bot-app.herokuapp.com/"
